f51b1d1fd953d2812615a31effae4b2c1c46b74b
Nicklas Hersen <nhersen@kth.se>
2016-05-09 13:21:37 +0200

    Add libGDX base project.

---
+++ desktop/src/com/JetLag/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.JetLag.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.JetLag.game.JetLag;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JetLag(), config);
+	}
+}
