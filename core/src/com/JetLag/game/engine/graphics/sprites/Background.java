84c277584cc5a4b6b630fb33e683f16b93053589
nhersen <nhersen@kth.se>
2016-05-13 18:04:38 +0200

    :book: Add Backround doc.

--- core/src/com/JetLag/game/engine/graphics/sprites/Background.java
+++ core/src/com/JetLag/game/engine/graphics/sprites/Background.java
@@ -42,24 +42,45 @@ public class Background {
         bgTexture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
     }
 
+    /**
+     * Sets the position of the region.
+     *
+     * @param x position along the x-axis.
+     * @param y position along the y-axis.
+     */
     public void setRegionPos(int x, int y) {
-        region.setRegionX(x);
-        region.setRegionX(y);
+        region.setRegion(x, y, region.getRegionWidth(), region.getRegionHeight());
 
     }
 
+    /**
+     * Sets the region size.
+     *
+     * @param width new region width.
+     * @param height new region height.
+     */
     public void setRegionSize(int width, int height) {
         if (width <= 0) throw new IllegalArgumentException("Illegal width");
         if (height <= 0) throw new IllegalArgumentException("Illegal height");
 
-        region.setRegionHeight(width);
-        region.setRegionWidth(height);
+        region.setRegion(region.getRegionX(), region.getRegionY(), width, height);
     }
 
+    /**
+     * Moves the region by a specified amount (in pixels).
+     *
+     * @param x amount along the x-axis.
+     * @param y amount along the y-axis.
+     */
     public void moveRegion(int x, int y) {
         region.setRegion(region.getRegionX() + x, region.getRegionY() + y, region.getRegionWidth(), region.getRegionHeight());
     }
 
+    /**
+     * Returns the texture region to draw,
+     *
+     * @return the texture region that should be drawn.
+     */
     public TextureRegion getRegion() { return region; }
 }
 
