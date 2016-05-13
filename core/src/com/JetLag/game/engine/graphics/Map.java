23899543dfcae357c46c4240a29135c5958a1cf7
nhersen <nhersen@kth.se>
2016-05-13 18:17:02 +0200

    :book: Add doc to Map.java.

--- core/src/com/JetLag/game/engine/graphics/Map.java
+++ core/src/com/JetLag/game/engine/graphics/Map.java
@@ -3,13 +3,15 @@ package com.JetLag.game.engine.graphics;
 import com.JetLag.game.JetLag;
 import com.JetLag.game.engine.BasicObject;
 import com.JetLag.game.engine.graphics.sprites.Background;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Set;
 
 /**
- * Created by nicklas on 5/13/16.
+ * Basic game map.
+ *
+ * @author Nicklas Hersen.
+ * @version 05.13.2016
  */
 public class Map {
     protected Background background;
@@ -17,18 +19,41 @@ public class Map {
     // Objects that won't be removed (ie. planets).
     protected Set<BasicObject> static_objects;
 
+    /**
+     * Creates a map with a specified background and region size.
+     * The region size specifies the part of the background image
+     * should be rendered.
+     *
+     * @param bgName background name.
+     * @param rwidth region width.
+     * @param rheight region height.
+     */
     public Map(String bgName, int rwidth, int rheight) {
         background = new Background(bgName, rwidth, rheight);
     }
 
+    /**
+     * Creates a map with the standard background and standard region size.
+     */
     public Map() {
         this("space-background.png", 512, 512);
     }
 
+    /**
+     * Moves the background region by a specified amount (in pixels).
+     *
+     * @param x amount along the x-axis.
+     * @param y amount along the y-axis.
+     */
     public void moveBackground(int x, int y) {
         background.moveRegion(x, y);
     }
 
+    /**
+     * Draw the background.
+     *
+     * @param sb spritebatch renderer to use.
+     */
     public void drawBackground(SpriteBatch sb) {
         sb.begin();
         sb.draw(background.getRegion(), 0, 0, JetLag.WIDTH, JetLag.HEIGHT);
