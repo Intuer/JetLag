baa2c9ce23d4979798aaa14834dc8e9a7c5ca3bf
sorme <sorme@kth.se>
2016-05-14 22:46:35 +0200

    :wrench: Small fixes to implementation of grid.
    Add Player2, a test version of player to show and test the movement of grid.

---
+++ core/src/com/JetLag/game/engine/graphics/sprites/Player2.java
@@ -0,0 +1,47 @@
+package com.JetLag.game.engine.graphics.sprites;
+
+import com.JetLag.game.engine.PhysObject;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by jacob_000 on 2016-05-14.
+ */
+
+public class Player2 extends BasicShape {
+
+    protected int radius;
+    protected int borderwidth = 10;
+    protected int shadewidth = 2;
+
+    public Player2(int x, int y, float mass, Vector3 vel, float[] colour) {
+        super(x, y, mass, vel, colour);
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void render(ShapeRenderer sr) {
+        //A black border
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+
+        sr.setColor(0,0,0,1);
+        sr.rect(pos.x-40, pos.y-40, 80,80);
+
+        //The actual circle base
+
+
+        sr.end();
+    }
+
+    public void setVelocity(Vector3 vel){
+        this.vel = vel;
+    }
+
+    public Vector3 getVelocity(){
+        return vel;
+    }
+}
