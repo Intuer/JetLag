877335238aa177ec02e347f4d0ca5451c03d84c7
sorme <sorme@kth.se>
2016-05-20 14:39:29 +0200

    Change menus

--- core/src/com/JetLag/game/engine/states/PauseState.java
+++ core/src/com/JetLag/game/engine/states/PauseState.java
@@ -1,9 +1,11 @@
 package com.JetLag.game.engine.states;
 
+import com.JetLag.game.JetLag;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import jdk.nashorn.internal.runtime.JSErrorType;
 
 /**
  * Created by jacob_000 on 2016-05-11.
@@ -11,9 +13,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class PauseState extends State {
 
     private Texture pausetext;
+    private Texture background;
 
     protected PauseState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("backgroundPlain.png");
         pausetext = new Texture("pausetext.png");
     }
 
@@ -34,7 +38,8 @@ public class PauseState extends State {
     @Override
     protected void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(pausetext,200,200);
+        sb.draw(background,0,0, JetLag.WIDTH, JetLag.HEIGHT);
+        sb.draw(pausetext, JetLag.WIDTH/2 - (pausetext.getWidth()/2),200);
         sb.end();
     }
 
