a0c389d32a7796a8362703293fed1b0976d8e6f0
Nicklas Hersen <nhersen@kth.se>
2016-05-22 13:44:35 +0200

    :book: Add player doc.

--- core/src/com/JetLag/game/engine/graphics/sprites/Player.java
+++ core/src/com/JetLag/game/engine/graphics/sprites/Player.java
@@ -1,63 +1,93 @@
 package com.JetLag.game.engine.graphics.sprites;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 /**
- * Created by jacob_000 on 2016-05-14.
+ * Basic player class.
+ *
+ * @author Nicklas Hersen, Jacob Sorme
+ * @version 22.05.2016
  */
 
 public class Player extends BasicSprite {
-
     protected float rotate = 0;
     private float length = 100;
     private Rectangle bounds;
     private TextureRegion region;
     private float scale = 0.5f;
 
+    /**
+     * Creates a basic player.
+     *
+     * @param x position along the x-axis.
+     * @param y position along the y-axis.
+     * @param mass player mass.
+     * @param vel initial player velocity.
+     */
     public Player(int x, int y, float mass, Vector3 vel) {
         super(x, y, mass, vel, "ship.png");
         region = new TextureRegion(texture);
         this.bounds = null;
     }
 
+    /**
+     * Draws the player.
+     *
+     * @param sb spritebatch to use.
+     */
     public void draw(SpriteBatch sb) {
         sb.begin();
         sb.draw(region, pos.x - texture.getWidth()/2, pos.y - texture.getHeight()/2, texture.getWidth()/2, texture.getHeight()/2, (float) texture.getWidth(), (float) texture.getHeight(), scale, scale, rotate + 180, false);
         sb.end();
     }
 
+    /**
+     * Sets the current rotation of the object, as in the
+     * direction the object is facing.
+     *
+     * @param rot new angle.
+     */
     public void setRotate(float rot){
         this.rotate = rot;
     }
 
+    /**
+     * Returns the current rotation in degrees.
+     *
+     * @return the current rotation in degrees.
+     */
     public float getRotate(){
         return rotate;
     }
 
+    /**
+     * Returns the current rotation in radians.
+     *
+     * @return rotation in radians.
+     */
     public float getRotateRad(){
         return getRotate()*(float)(Math.PI/180);
     }
 
-    public void setVelocity(Vector3 vel){
-        this.vel = vel;
-    }
-
-    public Vector3 getVelocity(){
-        return vel;
-    }
-
+    /**
+     * Updates the players position and velocity.
+     */
     public void update() {
-        float vel = getVelocityLen();
-
-        if (vel > 20) {
+        if (getVelocityLen()> 20) {
             getVelocity().nor().scl(20);
         }
+
+        addPosition(vel);
+        moveToBounds();
     }
 
+    /**
+     * Ensures that the player stays inside of the specified
+     * region. If no region (bound) is specified nothing is done.
+     */
     public void moveToBounds() {
         if (bounds == null) return;
 
