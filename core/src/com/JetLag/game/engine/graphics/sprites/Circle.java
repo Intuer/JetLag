78ce5394c2c432cd15b4c1e88169f2be440e24f0
nhersen <nhersen@kth.se>
2016-05-24 13:02:28 +0200

    :book: Add doc to Circle.java.

--- core/src/com/JetLag/game/engine/graphics/sprites/Circle.java
+++ core/src/com/JetLag/game/engine/graphics/sprites/Circle.java
@@ -1,12 +1,13 @@
 package com.JetLag.game.engine.graphics.sprites;
 
-import com.JetLag.game.engine.PhysObject;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector3;
 
 /**
- * Circle. A PhysObject and a BasicObject.
+ * A basic drawable circle with a hitbox.
+ *
+ * @author Jacob Sorme, Nicklas Hersen
+ * @version 05.24.2016
  */
 
 public class Circle extends BasicShape {
@@ -14,16 +15,36 @@ public class Circle extends BasicShape {
     protected int shadewidth = 2;
     protected com.badlogic.gdx.math.Circle hitbox;
 
+    /**
+     * Creates a basic drawable circle with the specified properties.
+     *
+     * @param x position along the x-axis.
+     * @param y position along the y-axis.
+     * @param mass circle mass.
+     * @param vel circle velocity.
+     * @param colour colour of the circle.
+     * @param radius radius of the circle.
+     */
     public Circle(int x, int y, float mass, Vector3 vel, float[] colour, int radius) {
         super(x, y, mass, vel, colour);
         hitbox = new com.badlogic.gdx.math.Circle(x, y, radius);
         size = radius;
     }
 
+    /**
+     * Returns the radius of the hitbox of the circle.
+     *
+     * @return the radius of the hit box.
+     */
     public float getRadius() {
         return hitbox.radius;
     }
 
+    /**
+     * Renders this circle using the specified ShapeRenderer.
+     *
+     * @param sr ShapeRenderer to use.
+     */
     public void render(ShapeRenderer sr) {
         //A black border
         sr.begin(ShapeRenderer.ShapeType.Filled);
@@ -38,6 +59,11 @@ public class Circle extends BasicShape {
         sr.end();
     }
 
+    /**
+     * Returns the hitbox of this circle.
+     *
+     * @return the hitbox of this circle.
+     */
     public final com.badlogic.gdx.math.Circle getHitbox() {
         return hitbox;
     }
