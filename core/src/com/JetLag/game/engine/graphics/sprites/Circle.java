39ee8496d9c08cae26d444ea5e8d34f8878fd657
nhersen <nhersen@kth.se>
2016-05-24 12:58:20 +0200

    :wrench: Add a hitbox (circle) to Circle.java.

--- core/src/com/JetLag/game/engine/graphics/sprites/Circle.java
+++ core/src/com/JetLag/game/engine/graphics/sprites/Circle.java
@@ -12,14 +12,16 @@ import com.badlogic.gdx.math.Vector3;
 public class Circle extends BasicShape {
     protected int borderwidth = 10;
     protected int shadewidth = 2;
+    protected com.badlogic.gdx.math.Circle hitbox;
 
     public Circle(int x, int y, float mass, Vector3 vel, float[] colour, int radius) {
         super(x, y, mass, vel, colour);
-        size =  radius;
+        hitbox = new com.badlogic.gdx.math.Circle(x, y, radius);
+        size = radius;
     }
 
-    public int getRadius() {
-        return size;
+    public float getRadius() {
+        return hitbox.radius;
     }
 
     public void render(ShapeRenderer sr) {
@@ -27,12 +29,16 @@ public class Circle extends BasicShape {
         sr.begin(ShapeRenderer.ShapeType.Filled);
 
         sr.setColor(0,0,0,1);
-        sr.circle(pos.x,pos.y,size);
+        sr.circle(pos.x, pos.y, hitbox.radius);
 
         //The actual circle base
         sr.setColor(colour[0],colour[1],colour[2],1);
-        sr.circle(pos.x,pos.y,size-borderwidth);
+        sr.circle(pos.x, pos.y, hitbox.radius - borderwidth);
 
         sr.end();
+    }
+
+    public final com.badlogic.gdx.math.Circle getHitbox() {
+        return hitbox;
     }
 }
